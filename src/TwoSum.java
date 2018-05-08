2ef0dffbfd32a660c67eda22a0979525def84e91
Jesus Villegas <jesus_villegas@intuit.com>
2018-05-08 13:57:19 -0700

    Skeet

---
+++ src/TwoSum.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> complements = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int curNum = nums[i];
+            if(complements.get(curNum) != null)
+                return new int[]{complements.get(curNum), i};
+            complements.put(target - curNum, i);
+        }
+        return null;
+    }
+}
