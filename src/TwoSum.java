01528822c8e34947a556b20256c0e52b14ebc9e7
Jesus Villegas <jesus_villegas@intuit.com>
2018-05-08 13:59:58 -0700

    no need for psvm

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,10 +1,6 @@
 import java.util.HashMap;
 
 public class TwoSum {
-    public static void main(String[] args) {
-        
-    }
-
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> complements = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
