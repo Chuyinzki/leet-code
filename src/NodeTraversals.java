fd3d37c7c8eb747533aff584bd0b445cc3adbf19
Jesus Villegas <jesus_villegas@intuit.com>
2019-04-30 19:14:27 -0700

    Minor fix and printing

--- src/NodeTraversals.java
+++ src/NodeTraversals.java
@@ -6,16 +6,33 @@ public class NodeTraversals {
     // 0, 1, 2, 3, null, 5, 6, 7, null, null, 10
     /*
     *                       0
-    *           1                         2
-    *       3      null                 5   6
-    *     7   null
+    *           1                            2
+    *       3      null                 5           6
+    *     7   null                  null  10
     * */
 
     public static void main(String[] args) {
         Integer[] arr = {0, 1, 2, 3, null, 5, 6, 7, null, null, 10};
         Node<Integer> root = createNodes(arr);
-        System.out.println();
 
+        System.out.println("In order traversal:");
+        inOrderTraverse(root);
+        spaceOut(5);
+
+    }
+
+    public static void spaceOut(int numTimes) {
+        for(int i = 0; i < numTimes; i++)
+            System.out.println();
+    }
+
+    public static <T> void inOrderTraverse(Node<T> root) {
+        if(root == null) {
+            return;
+        }
+        inOrderTraverse(root.left);
+        System.out.println(root.data);
+        inOrderTraverse(root.right);
     }
 
     public static <T> Node<T> createNodes(T[] objects) {
@@ -25,12 +42,16 @@ public class NodeTraversals {
         for (int i = 1; i < objects.length; i+=2) {
             Node<T> parent = insertOrdered.remove(0);
             Node<T> left = new Node<>(objects[i]);
-            insertOrdered.add(left);
-            parent.left = left;
+            if(left.data != null) {
+                insertOrdered.add(left);
+                parent.left = left;
+            }
             if(i + 1 < objects.length) {
                 Node<T> right = new Node<>(objects[i + 1]);
-                insertOrdered.add(right);
-                parent.right = right;
+                if(right.data != null) {
+                    insertOrdered.add(right);
+                    parent.right = right;
+                }
             }
         }
         return rootRet;
