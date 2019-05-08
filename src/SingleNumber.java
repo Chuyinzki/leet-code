cc5d22974d51bedcbd5011a7f4c3018fb71645f0
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-08 14:08:58 -0700

    Bitzz

---
+++ src/SingleNumber.java
@@ -0,0 +1,14 @@
+public class SingleNumber {
+
+    public static void main(String[] args) {
+        //[4,1,2,1,2] -> 4
+        singleNumber(new int[]{4,1,2,1,2});
+    }
+
+    public static int singleNumber(int[] nums) {
+        int ret = 0;
+        for(int i : nums)
+            ret = ret ^ i;
+        return ret;
+    }
+}
