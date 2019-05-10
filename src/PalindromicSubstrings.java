b79c1c2628ea69c0cb9a3bbeea7310bbe6f14cab
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-10 11:40:49 -0700

    slow af, but it works

---
+++ src/PalindromicSubstrings.java
@@ -0,0 +1,24 @@
+public class PalindromicSubstrings {
+
+    public static void main(String[] args) {
+        System.out.println(countSubstrings("aaa")); //should return 6
+    }
+
+    public static int countSubstrings(String s) {
+        if(s.length() == 1) return 1;
+        int palindromes = 0;
+        for(int i = 0; i < s.length(); i++) { //aaa
+            palindromes += isPalindrome(s.substring(0, i + 1)) ? 1 : 0;
+        }
+        return palindromes + countSubstrings(s.substring(1));
+    }
+
+    public static boolean isPalindrome(String s) {
+        if(s.length() == 1) return true;
+        for(int i = 0; i < s.length() / 2; i++) {
+            if(s.charAt(i) != s.charAt(s.length() - 1 -i))
+                return false;
+        }
+        return true;
+    }
+}
