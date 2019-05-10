6e3a340bff4f6a18b775f5a27e43bd658e52f982
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-10 12:39:15 -0700

    it was staring me in the face. I'm not drunk.

--- src/Codec.java
+++ src/Codec.java
@@ -4,7 +4,7 @@ import java.util.Map;
 public class Codec {
 
     private final static String MY_BASE = "http://breh.com/";
-    private final static Map<String, Integer> mappy = new HashMap<>();
+    private final static Map<Integer, String> mappy = new HashMap<>();
 
     public static void main(String[] args) {
         decode(encode("https://yourmom.com"));
@@ -12,24 +12,16 @@ public class Codec {
 
     // Encodes a URL to a shortened URL.
     public static String encode(String longUrl) {
-        if(mappy.get(longUrl) != null)
-            return MY_BASE + mappy.get(longUrl);
-        else {
-            int value = mappy.size() + 1;
-            mappy.put(longUrl, value);
-            return MY_BASE + value;
-        }
+        int value = mappy.size() + 1;
+        mappy.put(value, longUrl);
+        return MY_BASE + value;
+
     }
 
     // Decodes a shortened URL to its original URL.
     public static String decode(String shortUrl) {
         String valueString = shortUrl.replace(MY_BASE, "");
         Integer value = Integer.parseInt(valueString);
-        for (Map.Entry<String, Integer> entry : mappy.entrySet()) {
-            if (entry.getValue().equals(value)) {
-                return entry.getKey();
-            }
-        }
-        return null;
+        return mappy.get(value);
     }
 }
