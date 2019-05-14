eac7fcab183a08c9c8a0a05ce06a307eb0a52ffd
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-14 14:30:34 -0700

    The man, the legend

---
+++ src/InvertTree.java
@@ -0,0 +1,13 @@
+public class InvertTree {
+
+    public static void main(String[] args) {
+        invertTree(Util.getBasicTree());
+    }
+    public static TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode right = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(right);
+        return root;
+    }
+}
