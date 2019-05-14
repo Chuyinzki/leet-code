cdac80a084062658ea273e337e965bedd6e78d6e
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-14 12:02:48 -0700

    Let's develop a new approach

---
+++ src/RecostructQueue.java
@@ -0,0 +1,9 @@
+public class RecostructQueue {
+    public static void main(String[] args) {
+        reconstructQueue(new int[][]{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}});
+    }
+
+    public static int[][] reconstructQueue(int[][] people) {
+        return null;
+    }
+}
