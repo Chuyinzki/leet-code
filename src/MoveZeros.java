5af88a432ae73048ced306cca1a12327bfc51c6f
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-14 20:02:15 -0700

    format

--- src/MoveZeros.java
+++ src/MoveZeros.java
@@ -1,21 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class MoveZeros {
 
     public static void main(String[] args) {
-        moveZeroes(new int[]{0,1,0,3,12});
+        moveZeroes(new int[]{0, 1, 0, 3, 12});
     }
 
     public static void moveZeroes(int[] nums) {
         int j = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != 0) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
                 nums[j] = nums[i];
                 j++;
             }
         }
-        for(int i = j; i < nums.length; i++)
+        for (int i = j; i < nums.length; i++)
             nums[i] = 0;
     }
 }
