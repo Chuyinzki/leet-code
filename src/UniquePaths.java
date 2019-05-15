1c2063b8d7777e36a534a7a650f7dbe14c618443
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-15 11:39:17 -0700

    Speedy McSpeedyface

--- src/UniquePaths.java
+++ src/UniquePaths.java
@@ -1,7 +1,3 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-
 public class UniquePaths {
 
     public static void main(String[] args) {
@@ -9,28 +5,17 @@ public class UniquePaths {
     }
 
     public static int uniquePaths(int m, int n) {
-        if (n == 0 || m == 0) return 0;
-        return (int) uniquePathsHelper(m, n, new HashMap<>())[0];
-    }
-
-    public static Object[] uniquePathsHelper(int m, int n, HashMap<List<Integer>, Integer> map) {
-        if (n == 0 || m == 0) {
-            map.put(Arrays.asList(m, n), 0);
-            return new Object[]{0, map};
+        if(m == 0 || n == 0) return 0;
+        int[][] map = new int[m][n];
+        for(int i = m - 1; i >=0; i--) {
+            for(int j = n - 1; j>=0; j--) {
+                if(i + 1 >= map.length || j + 1 >= map[0].length) {
+                    map[i][j] = 1;
+                } else {
+                    map[i][j] = map[i + 1][j] + map[i][j + 1];
+                }
+            }
         }
-        if (m == 1 || n == 1) {
-            map.put(Arrays.asList(m, n), 1);
-            return new Object[]{1, map};
-        }
-        Integer curAnswer = map.get(Arrays.asList(m, n));
-        if(curAnswer == null)
-            curAnswer = map.get(Arrays.asList(n, m));
-        if (curAnswer != null)
-            return new Object[]{curAnswer, map};
-        Object[] ans1 = uniquePathsHelper(m - 1, n, map);
-        Object[] ans2 = uniquePathsHelper(m, n - 1, (HashMap<List<Integer>, Integer>) ans1[1]);
-        map = (HashMap<List<Integer>, Integer>) ans2[1];
-        map.put(Arrays.asList(m, n), (int) ans1[0] + (int) ans2[0]);
-        return new Object[]{(int) ans1[0] + (int) ans2[0], ans2[1]};
+        return map[0][0];
     }
 }
