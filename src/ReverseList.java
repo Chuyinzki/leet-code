d95db7424cc1708963704ab0323e5c93e84f50ac
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-15 08:52:36 -0700

    Reformat

--- src/ReverseList.java
+++ src/ReverseList.java
@@ -1,21 +1,19 @@
 import Objects.ListNode;
 
-import java.util.Stack;
-
 public class ReverseList {
 
     public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null) return head;
+        if (head == null || head.next == null) return head;
         ListNode prev = head;
         ListNode cur = head.next;
         ListNode next = cur.next;
         prev.next = null;
 
-        while(cur != null) {
+        while (cur != null) {
             cur.next = prev;
             prev = cur;
             cur = next;
-            if(next != null)
+            if (next != null)
                 next = next.next;
         }
         return prev;
