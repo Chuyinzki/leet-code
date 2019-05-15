266110c2af99e945fa10e9438a2e6d34c270c15a
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-14 20:18:45 -0700

    works, but can be faster

--- src/ReverseList.java
+++ src/ReverseList.java
@@ -1,7 +1,23 @@
 import Objects.ListNode;
 
-public class ReverseList {
-    public ListNode reverseList(ListNode head) {
+import java.util.Stack;
 
+public class ReverseList {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        Stack<ListNode> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head);
+            head = head.next;
+        }
+        ListNode returnHead = stack.pop();
+        head = returnHead;
+        while (!stack.isEmpty()) {
+            head.next = stack.pop();
+            head = head.next;
+        }
+        head.next = null;
+        return returnHead;
     }
 }
