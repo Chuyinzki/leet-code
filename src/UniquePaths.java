e82f8666f8c8c2d11a03eb892430ef59fd24548d
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-15 11:40:51 -0700

    Cleanup

--- src/UniquePaths.java
+++ src/UniquePaths.java
@@ -5,11 +5,11 @@ public class UniquePaths {
     }
 
     public static int uniquePaths(int m, int n) {
-        if(m == 0 || n == 0) return 0;
+        if (m == 0 || n == 0) return 0;
         int[][] map = new int[m][n];
-        for(int i = m - 1; i >=0; i--) {
-            for(int j = n - 1; j>=0; j--) {
-                if(i + 1 >= map.length || j + 1 >= map[0].length) {
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i + 1 >= map.length || j + 1 >= map[0].length) {
                     map[i][j] = 1;
                 } else {
                     map[i][j] = map[i + 1][j] + map[i][j + 1];
