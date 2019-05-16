1243b92767d948af604597f3ee4d5d0211ab3b7d
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-16 11:06:14 -0700

    Speedy mcquick

--- src/FindDuplicate.java
+++ src/FindDuplicate.java
@@ -1,10 +1,22 @@
 public class FindDuplicate {
 
     public int findDuplicate(int[] nums) {
-        int summation = 0;
-        for (int i = 1; i < nums.length; i++)
-            summation += i;
-        for (int num : nums) summation -= num;
-        return Math.abs(summation);
+        int i = 0, j = 0;
+        int intersection = 0;
+        while (true) {
+            i = nums[i];
+            j = nums[j];
+            j = nums[j];
+            if (nums[i] == nums[j]){
+                i = 0;
+                break;
+            }
+        }
+        while(true) {
+            if(nums[i] == nums[j])
+                return nums[j];
+            i = nums[i];
+            j = nums[j];
+        }
     }
 }
