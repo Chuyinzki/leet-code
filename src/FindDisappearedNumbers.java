5fdd01120a5845502d2a325c003213e577c49772
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-16 09:57:26 -0700

    Space efficient

---
+++ src/FindDisappearedNumbers.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindDisappearedNumbers {
+
+    public static void main(String[] args) {
+        findDisappearedNumbers(new int[]{1, 1});
+    }
+
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> missingInts = new ArrayList<>();
+        int lastNum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int curNum = nums[i];
+            if (lastNum != curNum)
+                for (int j = lastNum + 1; j < curNum; j++)
+                    missingInts.add(j);
+            lastNum = curNum;
+        }
+        for(int i = lastNum + 1; i <= nums.length; i++)
+            missingInts.add(i);
+        return missingInts;
+    }
+}
