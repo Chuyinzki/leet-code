d9da82e832e4e4e69e514aa2f0c69276eecf8968
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-16 15:21:26 -0700

    This works but it's too slow'

---
+++ src/WordBreak.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordBreak {
+
+    public static void main(String[] args) {
+        System.out.println(wordBreak("aaaaaaa", Arrays.asList("aaaa","aaa")));
+    }
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> dict = new HashSet<>(wordDict);
+        return wordBreakHelper(s, dict);
+    }
+
+    private static boolean wordBreakHelper(String s, Set<String> dict) {
+        if (s == null || s.isEmpty()) return false;
+        for (int i = 0; i < s.length(); i++) {
+            if (dict.contains(s.substring(0, i + 1))) {
+                if (i + 1 == s.length()) return true;
+                else {
+                    boolean attempt = wordBreakHelper(s.substring(i + 1), dict);
+                    if(attempt) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
