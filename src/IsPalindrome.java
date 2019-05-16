e876e65b5669a849c8c98926f96b0d6cd0e74eaf
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-16 13:03:20 -0700

    Oh boy

---
+++ src/IsPalindrome.java
@@ -0,0 +1,9 @@
+import Objects.ListNode;
+
+public class IsPalindrome {
+
+    //1->2->2->1 is true. Find O(n) time and O(1) space
+    public boolean isPalindrome(ListNode head) {
+        return false;
+    }
+}
