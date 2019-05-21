f921999ab18cc085b54c015d6a303091156611e0
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-21 15:43:12 -0700

    Will try this later

---
+++ src/SingleNumberII.java
@@ -0,0 +1,11 @@
+public class SingleNumberII {
+    public static void main(String[] args) {
+        //[4,1,2,1,2] -> 4
+        singleNumberII(new int[]{4, 1, 2, 1, 1, 2, 2});
+    }
+
+    //Sort and traverse would end as O(n log n)
+    public static int singleNumberII(int[] nums) {
+
+    }
+}
--- src/ValidPalindrome.java
+++ src/ValidPalindrome.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ValidPalindrome {
 
     public static void main(String[] args) {
