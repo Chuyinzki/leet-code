a08335bc2c25464f109a2aa472888f76e4c74e7f
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-21 16:41:13 -0700

    Finished test for equivalence

--- src/BubbleVsMergeSort.java
+++ src/BubbleVsMergeSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class BubbleVsMergeSort {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int[] toSort = new int[100000];
         Random rand = new Random();
         for(int i = 0; i < toSort.length; i++) {
@@ -14,10 +14,15 @@ public class BubbleVsMergeSort {
         long end1 = System.currentTimeMillis();
 
         long start2 = System.currentTimeMillis();
-        MergeSort.mergeSort(toSort2);
+        toSort2 = MergeSort.mergeSort(toSort2);
         long end2 = System.currentTimeMillis();
 
         System.out.printf("Bubble sort took %d ms\n", end1 - start1);
         System.out.printf("Merge sort took %d ms\n", end2 - start2);
+
+        for(int i = 0; i < toSort.length; i++)
+            if(toSort[i] != toSort2[i])
+                throw new Exception("Oh damn, the sorted outputs are not equivalent! Error at i = " + i);
+        System.out.println("The lists have finished testing for equivalence.");
     }
 }
