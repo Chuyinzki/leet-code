6b72c3e29745a9fe0feef437774371dfdb7f0c73
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-21 16:22:36 -0700

    bubble sortamundo

---
+++ src/BubbleSort.java
@@ -0,0 +1,21 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        for (int i : bubbleSort(new int[]{3, 6, 2, 7, 12, 9, 11, 5, 10, 12})) {
+            System.out.print(i + ", ");
+        }
+    }
+
+    public static int[] bubbleSort(int[] toSort) {
+        int i = toSort.length - 2;
+        while(i >= 0) {
+            int running = i;
+            while(running + 1 < toSort.length && toSort[running] >= toSort[running + 1]){
+                int temp = toSort[running];
+                toSort[running] = toSort[running + 1];
+                toSort[++running] = temp;
+            }
+            i--;
+        }
+        return toSort;
+    }
+}
