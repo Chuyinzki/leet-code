85a92dee5c21fd855031a94319848281e1aac350
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-21 15:25:41 -0700

    This is first attempt

---
+++ src/ValidPalindrome.java
@@ -0,0 +1,21 @@
+public class ValidPalindrome {
+
+    public static void main(String[] args) {
+        System.out.print(isPalindrome("A man, a plan, a canal: Panama")); //Should return true
+    }
+
+    public static boolean isPalindrome(String toCheck) {
+        int start = 0;
+        int end = toCheck.length() - 1;
+        while(start < end) {
+            //move up
+            while(start < toCheck.length() && !Character.isAlphabetic(toCheck.charAt(start))) start++;
+            while(end >= 0 && !Character.isAlphabetic(toCheck.charAt(end))) end--;
+            int diff = Math.abs(toCheck.charAt(end) - toCheck.charAt(start));
+            if(diff != 0 && diff != 32) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
