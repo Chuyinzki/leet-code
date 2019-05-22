3c2b02ddcd315e010c92cb95576f5a7e2d208075
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-21 18:37:45 -0700

    This is a solid plan.

---
+++ src/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,23 @@
+public class IntersectionOfTwoLinkedLists {
+    /*
+    Write a program to find the node at which the intersection of two singly linked lists begins.
+
+    For example, the following two linked lists:
+
+    A:          a1 → a2
+                       ↘
+                         c1 → c2 → c3
+                       ↗
+    B:     b1 → b2 → b3
+
+    begin to intersect at node c1.
+    */
+
+    //This is the plan:
+    // 1. run to the end (c3)
+    // 2. create a cycle from c3 to b1.
+    // 3. use the algorithm we made to find the beginning of the cycle
+    // 4. Remove the cycle (c3.next = null)
+    // 5. Return our saved beginning of the cycle (c1)
+
+}
