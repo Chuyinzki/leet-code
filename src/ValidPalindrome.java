3ef003bd05c2c75f06254f3b82a265af15760700
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-21 19:43:25 -0700

    This is for numbers allowed

--- src/ValidPalindrome.java
+++ src/ValidPalindrome.java
@@ -1,7 +1,7 @@
 public class ValidPalindrome {
 
     public static void main(String[] args) {
-        System.out.print(isPalindrome("A man, a plan, a canal: Panama")); //Should return true
+        System.out.print(isPalindrome("0P")); //Should return true
     }
 
     public static boolean isPalindrome(String s) {
@@ -9,11 +9,13 @@ public class ValidPalindrome {
         int end = s.length() - 1;
         while (start < end) {
             //move up
-            while (start < s.length() && !Character.isAlphabetic(s.charAt(start))) start++;
-            while (end >= 0 && !Character.isAlphabetic(s.charAt(end))) end--;
-            if (start > end) return false;
+            while (start < s.length() && !Character.isAlphabetic(s.charAt(start)) && !Character.isDigit(s.charAt(start))) start++;
+            while (end >= 0 && !Character.isAlphabetic(s.charAt(end)) && !Character.isDigit(s.charAt(end))) end--;
+            if (start > end) return true;
             int diff = Math.abs(s.charAt(end) - s.charAt(start));
-            if (diff != 0 && diff != 32) return false;
+            if (start <= 122 && start >= 65 && end <= 122 && end >= 54) {
+                if(diff != 0 && diff != 32) return false;
+            } else if(diff != 0) return false;
             start++;
             end--;
         }
