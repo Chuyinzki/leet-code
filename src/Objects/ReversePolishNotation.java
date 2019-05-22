2dcc375d871afcdc646fea445a62a381f1b6357e
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-21 17:45:14 -0700

    Get ready for naive attempt

---
+++ src/Objects/ReversePolishNotation.java
@@ -0,0 +1,11 @@
+package Objects;
+
+public class ReversePolishNotation {
+    public static void main(String[] args) {
+        System.out.print(reversePolishNotation(new String[]{"2", "1", "+", "3", "*"}));
+    }
+
+    public static int reversePolishNotation(String[] toDo) {
+        return 0;
+    }
+}
