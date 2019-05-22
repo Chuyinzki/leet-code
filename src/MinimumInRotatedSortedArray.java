d4bcc655c7ca6b599fb8d474c1351ed3b71700bd
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-21 18:16:36 -0700

    I Thiiiink that's what this question is asking for

---
+++ src/MinimumInRotatedSortedArray.java
@@ -0,0 +1,13 @@
+public class MinimumInRotatedSortedArray {
+
+    public static void main(String[] args) {
+        System.out.println(findMinInRotatedSortedArray(new int[]{4,5,6,7,0,1,2}));
+    }
+
+    public static int findMinInRotatedSortedArray(int[] rotatedArray) {
+        for(int i = 0; i < rotatedArray.length - 1; i++)
+            if(rotatedArray[i + 1] < rotatedArray[i])
+                return rotatedArray[i + 1];
+        return rotatedArray[0];
+    }
+}
