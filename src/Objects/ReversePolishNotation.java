7ca015145bd5e3f3548ebb9c07690c2ccdd489a5
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-21 18:01:50 -0700

    This is working I think

--- src/Objects/ReversePolishNotation.java
+++ src/Objects/ReversePolishNotation.java
@@ -1,11 +1,38 @@
 package Objects;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 public class ReversePolishNotation {
     public static void main(String[] args) {
         System.out.print(reversePolishNotation(new String[]{"2", "1", "+", "3", "*"}));
     }
 
-    public static int reversePolishNotation(String[] toDo) {
-        return 0;
+    public static int reversePolishNotation(String[] todo) {
+        Stack<Integer> numbers = new Stack<>();
+        Set<String> operators = new HashSet<>();
+        operators.add("+");
+        operators.add("-");
+        operators.add("/");
+        operators.add("*");
+        for (String nextString : todo) {
+            if (!Character.isDigit(nextString.charAt(0))) {
+                Integer second = numbers.pop();
+                Integer first = numbers.pop();
+                numbers.add(doArithmetic(first, nextString.charAt(0), second));
+            } else {
+                numbers.add(Integer.parseInt(nextString));
+            }
+        }
+        return numbers.pop();
+    }
+
+    private static int doArithmetic(int a, char op, int b) {
+        if (op == '+') return a + b;
+        else if (op == '-') return a - b;
+        else if (op == '/') return a / b;
+        else //op == '*'
+            return a * b;
     }
 }
