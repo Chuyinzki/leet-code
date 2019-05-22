7fbbba50dc6649e8e48d55b17a95a25a30489a25
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-22 14:00:47 -0700

    Quite, quite

---
+++ src/Objects/RecentCounter.java
@@ -0,0 +1,17 @@
+package Objects;
+
+import java.util.LinkedList;
+
+public class RecentCounter {
+    final LinkedList<Integer> pings;
+
+    public RecentCounter() {
+        pings = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        pings.add(t);
+        while (pings.peek() < t - 3000) pings.poll();
+        return pings.size();
+    }
+}
