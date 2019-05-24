902b74a3f6ef79caf69655bb6995403d31a17147
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-24 11:54:40 -0700

    This was accepted, but there is a better way

---
+++ src/CountPrimes.java
@@ -0,0 +1,18 @@
+public class CountPrimes {
+
+    //Sieve
+    public int countPrimes(int n) {
+        if(n < 2) return 0;
+        boolean[] arr = new boolean[n];
+        for(int i = 2; i < n; i++) {
+            if(arr[i]) continue;
+            for(int j = i + i; j < arr.length; j+=i) {
+                arr[j] = true;
+            }
+        }
+        int ret = 0;
+        for(int i = 2; i < arr.length; i++)
+            if(!arr[i]) ret++;
+        return ret;
+    }
+}
