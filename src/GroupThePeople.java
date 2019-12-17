cc606acd5ee330244e7681f83fc7cbe095550620
Jesus Villegas <jesus_villegas@intuit.com>
2019-12-17 09:18:17 -0800

    Best space, 96% speed

---
+++ src/GroupThePeople.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupThePeople {
+
+    public static void main(String[] args) {
+        groupThePeople(new int[]{3,3,3,3,3,1,3});
+    }
+
+    public static List<List<Integer>> groupThePeople(int[] groupSizes) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        List<List<Integer>> retList = new ArrayList<>();
+        for (int i = 0; i < groupSizes.length; i++) {
+            int size = groupSizes[i];
+            List<Integer> temp = map.get(size);
+            if(temp == null)
+                temp = new ArrayList<>();
+            temp.add(i);
+            if(temp.size() == size) {
+                retList.add(new ArrayList(temp));
+                temp.clear();
+            }
+            map.put(size, temp);
+        }
+        return retList;
+    }
+}
