3f9c6022a025dad7f45409746a9360061bbf7f5f
Jesus Villegas <jesus_villegas@intuit.com>
2019-12-17 11:55:22 -0800

    Rekt

--- src/SubtractProductAndSum.java
+++ src/SubtractProductAndSum.java
@@ -1,12 +1,14 @@
 public class SubtractProductAndSum {
+
     public int subtractProductAndSum(int n) {
-        String str = String.valueOf(n);
         int sum = 0;
         int mult = 1;
-        for(int i = 0; i < str.length(); i++) {
-            int val = Integer.parseInt(str.charAt(i) + "");
-            sum += val;
-            mult *= val;
+        int rem = n;
+        while (rem > 0) {
+            int temp = rem % 10;
+            sum += temp;
+            mult *= temp;
+            rem = rem / 10;
         }
         return mult - sum;
     }
