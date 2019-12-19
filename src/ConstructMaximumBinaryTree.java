83b04a7d598af3e3623a0096b0527bae50b4bc1d
Jesus Villegas <jesus_villegas@intuit.com>
2019-12-19 10:41:48 -0800

    Fastest and spaciest

--- src/ConstructMaximumBinaryTree.java
+++ src/ConstructMaximumBinaryTree.java
@@ -4,25 +4,25 @@ import java.util.Arrays;
 public class ConstructMaximumBinaryTree {
 
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return createMaxTreeHelper(nums);
+        return createMaxTreeHelper(nums, 0 , nums.length);
     }
 
-    private TreeNode createMaxTreeHelper(int[] nums) {
-        if(nums.length == 1)
-            return new TreeNode(nums[0]);
-        int indexOfMax = findMaxIndex(nums);
+    private TreeNode createMaxTreeHelper(int[] nums, int start, int end) {
+        int indexOfMax = findMaxIndex(nums, start, end);
+        if(indexOfMax == -1)
+            return null;
         TreeNode root = new TreeNode(nums[indexOfMax]);
-        if(indexOfMax > 0 && indexOfMax <= nums.length - 1)
-            root.left = createMaxTreeHelper(Arrays.copyOfRange(nums, 0, indexOfMax));
-        if(indexOfMax <= nums.length - 2)
-            root.right = createMaxTreeHelper(Arrays.copyOfRange(nums, indexOfMax + 1, nums.length));
+        if(indexOfMax > start)
+            root.left = createMaxTreeHelper(nums, start, indexOfMax);
+        if(indexOfMax <= end - 1)
+            root.right = createMaxTreeHelper(nums, indexOfMax + 1, end);
         return root;
     }
 
-    private int findMaxIndex(int[] nums) {
-        int maxIndex = 0;
-        int maxCurNum = 0;
-        for(int i = 0; i < nums.length; i++)
+    private int findMaxIndex(int[] nums, int start, int end) {
+        int maxIndex = -1;
+        int maxCurNum = -1;
+        for(int i = start; i < end; i++)
             if(nums[i] > maxCurNum) {
                 maxIndex = i;
                 maxCurNum = nums[i];
