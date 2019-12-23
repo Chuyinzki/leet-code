ef197b543979954ebcc735c57a6937eb26bc2e72
Jesus Villegas <jesus_villegas@intuit.com>
2019-12-23 11:57:45 -0800

    Cleanup

--- src/ConstructMaximumBinaryTree.java
+++ src/ConstructMaximumBinaryTree.java
@@ -1,32 +1,35 @@
 import Objects.TreeNode;
-import java.util.Arrays;
 
 public class ConstructMaximumBinaryTree {
 
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return createMaxTreeHelper(nums, 0 , nums.length);
+        return createMaxTreeHelper(nums, 0, nums.length);
     }
 
     private TreeNode createMaxTreeHelper(int[] nums, int start, int end) {
         int indexOfMax = findMaxIndex(nums, start, end);
-        if(indexOfMax == -1)
+        if (indexOfMax == -1) {
             return null;
+        }
         TreeNode root = new TreeNode(nums[indexOfMax]);
-        if(indexOfMax > start)
+        if (indexOfMax > start) {
             root.left = createMaxTreeHelper(nums, start, indexOfMax);
-        if(indexOfMax <= end - 1)
+        }
+        if (indexOfMax <= end - 1) {
             root.right = createMaxTreeHelper(nums, indexOfMax + 1, end);
+        }
         return root;
     }
 
     private int findMaxIndex(int[] nums, int start, int end) {
         int maxIndex = -1;
         int maxCurNum = -1;
-        for(int i = start; i < end; i++)
-            if(nums[i] > maxCurNum) {
+        for (int i = start; i < end; i++) {
+            if (nums[i] > maxCurNum) {
                 maxIndex = i;
                 maxCurNum = nums[i];
             }
+        }
         return maxIndex;
     }
 }
